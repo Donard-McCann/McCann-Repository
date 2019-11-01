7379e6e3dd955442d362ce0efb64b61b7af633fe
Donard-McCann <57262691+Donard-McCann@users.noreply.github.com>
2019-11-01 15:03:29 +0000

    Add files via upload

---
+++ ElectricalUnitMonitorsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using IntelliHome.Data.Models;
+using System.Linq;
+
+namespace IntelliHome.Web.Models
+{
+    public class ElectricalUnitMonitorsViewModel
+    {
+        public IList<ElectricalUnitMonitor> ElectricalUnitMonitors { get; set; }
+        public IList<ElectricalUnit> ElectricalUnits { get; set; }
+        public IList<Room> Rooms { get; set; }
+
+        public double TotalHouseCost => Rooms.Select(r => r.UsageCost).Sum();
+        public double TotalHouseUsage => Rooms.Select(r => r.UsagekWh).Sum();
+        public double TotalCo2Kg => TotalHouseUsage * 0.281;
+    }
+}
